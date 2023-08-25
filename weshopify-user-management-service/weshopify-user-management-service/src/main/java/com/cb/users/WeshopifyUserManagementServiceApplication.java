57a09644e4fd3a7137bdaafbc6cf7cd0a65d6f43
Dhruv.k <dhruv.k@digitelenetworks.com>
2023-08-25 18:11:27 +0530

    initial commit

---
+++ weshopify-user-management-service/weshopify-user-management-service/src/main/java/com/cb/users/WeshopifyUserManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cb.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeshopifyUserManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeshopifyUserManagementServiceApplication.class, args);
+    }
+
+}
