2201789f8d541cfb0fcd4bb96167e62cf1481c39
Dhruv.k <dhruv.k@digitelenetworks.com>
2024-01-24 15:24:29 +0530

    created service registry

---
+++ weshopify-user-management-service/weshopify-service-registry/src/main/java/org/cb/WeshopifyServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package org.cb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeshopifyServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeshopifyServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ weshopify-user-management-service/weshopify-service-registry/src/test/java/org/cb/WeshopifyServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeshopifyServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
