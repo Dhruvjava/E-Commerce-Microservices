8e494ecf04bfd0cad0f40928a141658c3fab2162
Dhruv.k <dhruv.k@digitelenetworks.com>
2024-01-30 18:12:19 +0530

    Impemented Configuration and added eureka client config also

--- weshopify-user-management-service/weshopify-authn-service/src/main/java/org/cb/WeshopifyAuthnServiceApplication.java
+++ weshopify-user-management-service/weshopify-authn-service/src/main/java/org/cb/WeshopifyAuthnServiceApplication.java
@@ -2,8 +2,10 @@ package org.cb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WeshopifyAuthnServiceApplication {
 
     public static void main(String[] args) {
