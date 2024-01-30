8cafbe7288036d0e8debd2b68da76782b5306162
Dhruv.k <dhruv.k@digitelenetworks.com>
2024-01-30 17:46:04 +0530

    initial commit

---
+++ weshopify-user-management-service/weshopify-authn-service/src/main/java/org/cb/WeshopifyAuthnServiceApplication.java
@@ -0,0 +1,13 @@
+package org.cb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeshopifyAuthnServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeshopifyAuthnServiceApplication.class, args);
+    }
+
+}
