54d2899554628a9e48aa97709fb9554513a5012b
Dhruv.k <dhruv.k@digitelenetworks.com>
2024-02-05 15:49:10 +0530

    modified swagger-ui config

--- weshopify-user-management-service/weshopify-user-management-service/src/main/java/com/cb/WeshopifyUserManagementServiceApplication.java
+++ weshopify-user-management-service/weshopify-user-management-service/src/main/java/com/cb/WeshopifyUserManagementServiceApplication.java
@@ -20,36 +20,17 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Weshopify User management SVC", version = "1.0",
-        description = "This is the Weshopify User management SVC API is for internal use",
-        contact = @Contact(name = "CodeBrain Pvt. Ltd.",
-                url = "http://www.codebrain.com/",
-                email = "mailto:info@codebrain.com"),
-        license = @License(
-                name = "MIT License",
-                url = "http://www.opensource.org/licenses/mit-license"
-        ),
-        termsOfService = "Weshopify Term & Condkitions"
-),
-        servers = {
-                @Server(
-                        description = "Weshopify Local Environment",
-                        url = "http://localhost:8061/dev"
-                )
-        },
-        security = {
-                @SecurityRequirement(
-                        name = "BearerAuth"
-                )
-        }
-)
-@SecurityScheme(
-        name = "BearerAuth",
-        description = "JWT Authorization",
-        bearerFormat = "JWT",
-        scheme = "bearer",
-        type = SecuritySchemeType.HTTP,
-        in = SecuritySchemeIn.HEADER
-)
+                description = "This is the Weshopify User management SVC API is for internal use",
+                contact = @Contact(name = "CodeBrain Pvt. Ltd.", url = "http://www.codebrain.com/",
+                                email = "mailto:info@codebrain.com"),
+                license = @License(name = "MIT License",
+                                url = "http://www.opensource.org/licenses/mit-license"),
+                termsOfService = "Weshopify Term & Condkitions"),
+                servers = {@Server(description = "Weshopify Local Environment",
+                                url = "http://localhost:8061/dev")},
+                security = {@SecurityRequirement(name = "BearerAuth")})
+@SecurityScheme(name = "BearerAuth", description = "JWT Authorization", bearerFormat = "JWT",
+                scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 @EnableDiscoveryClient
 @EnableFeignClients
 public class WeshopifyUserManagementServiceApplication {
@@ -66,7 +47,7 @@ public class WeshopifyUserManagementServiceApplication {
     @Bean(name = "errorProperties")
     public MessageSource errorProperties() {
         ReloadableResourceBundleMessageSource messageSource =
-                new ReloadableResourceBundleMessageSource();
+                        new ReloadableResourceBundleMessageSource();
         messageSource.setBasenames("classpath:/bundles/application_errors");
         // messageSource.setCacheSeconds(10); //reload messages every 10 seconds
         return messageSource;
@@ -75,26 +56,26 @@ public class WeshopifyUserManagementServiceApplication {
     @Bean(name = "messageProperties")
     public MessageSource messageProperties() {
         ReloadableResourceBundleMessageSource messageSource =
-                new ReloadableResourceBundleMessageSource();
+                        new ReloadableResourceBundleMessageSource();
         messageSource.setBasenames("classpath:/bundles/application_messages");
         // messageSource.setCacheSeconds(10); //reload messages every 10 seconds
         return messageSource;
     }
 
 
-//    @Bean(name = "reportProperties")
-//    public MessageSource reportProperties() {
-//        ReloadableResourceBundleMessageSource messageSource =
-//                new ReloadableResourceBundleMessageSource();
-//        messageSource.setBasenames("classpath:/bundles/report_templates");
-//        // messageSource.setCacheSeconds(10); //reload messages every 10 seconds
-//        return messageSource;
-//    }
+    //    @Bean(name = "reportProperties")
+    //    public MessageSource reportProperties() {
+    //        ReloadableResourceBundleMessageSource messageSource =
+    //                new ReloadableResourceBundleMessageSource();
+    //        messageSource.setBasenames("classpath:/bundles/report_templates");
+    //        // messageSource.setCacheSeconds(10); //reload messages every 10 seconds
+    //        return messageSource;
+    //    }
 
     @Bean(name = "emailProperties")
     public MessageSource emailProperties() {
         ReloadableResourceBundleMessageSource messageSource =
-                new ReloadableResourceBundleMessageSource();
+                        new ReloadableResourceBundleMessageSource();
         messageSource.setBasenames("classpath:/bundles/email_templates");
         // messageSource.setCacheSeconds(10); //reload messages every 10 seconds
         return messageSource;
