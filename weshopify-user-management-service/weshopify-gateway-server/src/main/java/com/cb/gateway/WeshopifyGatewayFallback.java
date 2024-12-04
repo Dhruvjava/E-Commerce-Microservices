a73fb35fc2e9ef08e0b90a8774ff22f230c47a4a
Dhruv Kumar <dhruv.k@digitelenetworks.com>
2024-12-04 14:49:55 +0530

    Implementing Spring cloud gateway and configured CircuitBreaker

--- weshopify-user-management-service/weshopify-gateway-server/src/main/java/com/cb/gateway/WeshopifyGatewayFallback.java
+++ weshopify-user-management-service/weshopify-gateway-server/src/main/java/com/cb/gateway/WeshopifyGatewayFallback.java
@@ -1,14 +1,14 @@
-package com.cb.circuit;
+package com.cb.gateway;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-@RestController("/api/gateway")
+@RestController
 public class WeshopifyGatewayFallback {
 
-  @GetMapping("/authn/failed")
-  public Mono<?> authnFallback() {
+  @GetMapping("/auth-fallback")
+  public Mono<String> authnFallback() {
 
     return Mono.just(
         "Our Authentication service is undergoing maintenance. Please try again in a few minutes. Thank you for your patience.");
